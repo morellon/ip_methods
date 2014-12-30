12d486cb7a982780dbefc60e0f8b481e0807bd38
Lucas Uyezu <lucas.uyezu@gmail.com>
2014-12-30 18:26:30 -0200

    Removing multiline anchors

--- lib/ip_methods/ipv6.rb
+++ lib/ip_methods/ipv6.rb
@@ -4,13 +4,13 @@ module IpMethods
   module IPv6
     TOTAL_BIT_SIZE = 128
     GROUP_BIT_SIZE = 16
-    IPV6_PATTERN = /^(?:[\da-f]{1,4}:){7}[\da-f]{1,4}$/i
-    
+    IPV6_PATTERN = /\A(?:[\da-f]{1,4}:){7}[\da-f]{1,4}\z$/i
+
     def self.uncompress(ip)
       missing_groups = (TOTAL_BIT_SIZE/GROUP_BIT_SIZE) - ip.gsub(/[^:]/, "").size
       ip.sub(/^:/, "0:").sub(/:$/, ":0").sub(/::/, ":0" * missing_groups + ":")
     end
-    
+
     def self.ipv6?(ip)
       !! uncompress(ip) =~ IPV6_PATTERN
     end
